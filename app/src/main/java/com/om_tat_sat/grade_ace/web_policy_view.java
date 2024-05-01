01f5b0ab52a1107cb52d3b5534b8db93ae3ca472
ASHISH SAHU <supershor.cp@gmail.com>
2024-05-01 16:09:56 +0530

    policy webview added

---
+++ app/src/main/java/com/om_tat_sat/grade_ace/web_policy_view.java
@@ -0,0 +1,33 @@
+package com.om_tat_sat.grade_ace;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class web_policy_view extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_web_policy_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        getWindow().setStatusBarColor(ContextCompat.getColor(web_policy_view.this,R.color.black));
+        webView=findViewById(R.id.web_policy_view);
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://drive.google.com/file/d/1ayPTzh_9EKUugo7l648y3M5TkFJBWnHi/view?usp=sharing");
+    }
+}
