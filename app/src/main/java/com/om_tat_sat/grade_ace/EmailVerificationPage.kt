4ab89ad4fdec075aa818db413cbde9428e8a0db3
ASHISH SAHU <supershor.cp@gmail.com>
2025-01-30 20:41:06 +0530

    Added 3.5 sec wait at first page with new ui, added login and signup backend code with email verification checking mandatory also added email verification page with appropriate animation.

---
+++ app/src/main/java/com/om_tat_sat/grade_ace/EmailVerificationPage.kt
@@ -0,0 +1,85 @@
+package com.om_tat_sat.grade_ace
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class EmailVerificationPage : AppCompatActivity() {
+    val mauth= FirebaseAuth.getInstance()
+    lateinit var linkVerification:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_email_verification_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<TextView>(R.id.logout_email_verification_page).setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                mauth.signOut()
+                startActivity(Intent(this@EmailVerificationPage,SecondLoadingPage::class.java))
+                finishAffinity()
+            }
+        })
+
+        linkVerification=findViewById<TextView>(R.id.resend_link_email_verification_page)
+
+        findViewById<TextView>(R.id.emailAddress).text=mauth.currentUser?.email
+
+        linkVerification.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                if(linkVerification.text==getString(R.string.resend_link)){
+                    mauth.currentUser?.sendEmailVerification()?.addOnSuccessListener {
+                        Toast.makeText(this@EmailVerificationPage, getString(R.string.verification_email_sent), Toast.LENGTH_SHORT).show()
+                        blockVerificationLink(30)
+
+                    }?.addOnFailureListener {
+                        Toast.makeText(this@EmailVerificationPage, getString(R.string.verification_email_failed), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+        })
+        keepChecking()
+    }
+    private fun blockVerificationLink(sec:Int){
+        if(sec==0){
+            linkVerification.isEnabled=true
+            linkVerification.text=getString(R.string.resend_link)
+        }
+        else {
+            linkVerification.isEnabled = false
+            linkVerification.text = "${getString(R.string.resend_link)} $sec"
+            Handler().postDelayed(Runnable {
+                blockVerificationLink(sec - 1)
+            }, 1000)
+        }
+    }
+    private fun keepChecking(){
+        mauth.currentUser?.reload()?.addOnSuccessListener {
+            if(mauth.currentUser?.isEmailVerified==true){
+                Log.d("TAG", "Email Verified")
+                // TODO start the main activity
+            } else {
+                Handler().postDelayed(Runnable {
+                    keepChecking()
+                },3000)
+            }
+        }?.addOnFailureListener{
+            Handler().postDelayed(Runnable {
+                keepChecking()
+            },1500)
+        }
+    }
+}
--- app/src/main/java/com/om_tat_sat/grade_ace/FirstLoadingPage.kt
+++ app/src/main/java/com/om_tat_sat/grade_ace/FirstLoadingPage.kt
@@ -1,5 +1,6 @@
 package com.om_tat_sat.grade_ace
 
+import android.content.Intent
 import android.media.tv.TsResponse
 import android.os.Bundle
 import android.os.Handler
@@ -33,36 +34,44 @@ class FirstLoadingPage : AppCompatActivity() {
 //            Log.e("AuthCheck", "Failed to authenticate user.", it)
 //        }
 
+        Handler().postDelayed(Runnable {
+            check()
+        },3500)
+    }
+    private fun check(){
         if(mauth.currentUser?.uid != null) {
             mauth.currentUser?.reload()?.addOnSuccessListener {
                 Log.e("AuthCheck", "User refresh done")
                 Log.d("AuthCheck", "${mauth.currentUser?.uid.toString()}")
                 Log.d("AuthCheck", "${mauth.currentUser?.isEmailVerified.toString()}")
                 if(mauth.currentUser?.isEmailVerified==true){
-                    // TODO start the activity
+                    // TODO start the main activity
                 } else {
                     mauth.currentUser?.sendEmailVerification()?.addOnSuccessListener {
+                        Toast.makeText(this, getString(R.string.verification_email_sent_with_email_verification_request), Toast.LENGTH_SHORT).show()
                         Log.d("AuthCheck", "Verification email sent successfully.")
-                        Toast.makeText(this, "Verification email sent successfully.", Toast.LENGTH_SHORT).show()
-                        Handler().postDelayed(Runnable {
-                            Toast.makeText(this, "Please verify your email.", Toast.LENGTH_SHORT).show()
-                        },3000)
+                        startActivity(Intent(this,EmailVerificationPage::class.java))
+                        finishAffinity()
                     }?.addOnFailureListener {
                         Log.e("AuthCheck", "Failed to send verification email.", it)
-                        Toast.makeText(this, "Failed to send verification email.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, getString(R.string.verification_email_failed_with_login_again_request), Toast.LENGTH_SHORT).show()
+                        mauth.signOut()
+                        startActivity(Intent(this,SecondLoadingPage::class.java))
+                        finishAffinity()
                     }
                     Log.d("AuthCheck", "User is authenticated but email is not verified.")
-                    // TODO go to verification page
                 }
             }?.addOnFailureListener{
                 Log.e("AuthCheck", "Failed to refresh user.", it)
-                // TODO go to login page
+                Toast.makeText(this, getString(R.string.refresh_user_failed), Toast.LENGTH_SHORT).show()
+                mauth.signOut()
+                startActivity(Intent(this,SecondLoadingPage::class.java))
+                finishAffinity()
             }
         } else {
             Log.d("AuthCheck", "User is not LoggedIn.")
-            // TODO go to login page
+            startActivity(Intent(this,SecondLoadingPage::class.java))
+            finishAffinity()
         }
-
-
     }
 }
